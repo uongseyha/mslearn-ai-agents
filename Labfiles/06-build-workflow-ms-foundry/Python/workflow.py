61453d814e60935aeb31e44840a1fd390283e798
Seyha Uong <uong.seyha@gmail.com>
2026-06-02 11:08:36 -0400

    workflow

--- Labfiles/06-build-workflow-ms-foundry/Python/workflow.py
+++ Labfiles/06-build-workflow-ms-foundry/Python/workflow.py
@@ -2,6 +2,8 @@ import os, json, re
 from dotenv import load_dotenv
 
 # Add references
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
 
 
 def print_workflow_output(output_text):
@@ -26,16 +28,40 @@ load_dotenv()
 endpoint = os.environ["PROJECT_ENDPOINT"]
 
 # Connect to the AI Project client
+with (
+    DefaultAzureCredential() as credential,
+    AIProjectClient(endpoint=endpoint, credential=credential) as project_client,
+    project_client.get_openai_client() as openai_client,
+):
 
 
     # Specify the workflow
+    workflow = {
+        "name": "ContosoPay-Customer-Support-Triage"
+    }
     
 
-    # Create a conversation and run the workflow
+     # Create a conversation and run the workflow
+    conversation = openai_client.conversations.create()
+    print(f"Created conversation (id: {conversation.id})")
+
+    stream = openai_client.responses.create(
+        conversation=conversation.id,
+        extra_body={"agent_reference" : {"name" : workflow["name"], "type": "agent_reference"}},
+        input="Start",
+        stream=True,
+    )
 
 
     # Process events from the workflow run
+    for event in stream:
+        if (event.type == "response.completed"):
+            print("\nResponse completed:")
+            response = openai_client.responses.retrieve(event.response.id)
+            print_workflow_output(response.output_text)
  
 
     # Clean up resources
+    openai_client.conversations.delete(conversation_id=conversation.id)
+    print("\nConversation deleted")
 
